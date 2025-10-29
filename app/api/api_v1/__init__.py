d7b750ba654dae13d30c8f89ea9be97726a67117
HELAFUG <loxand42@gmail.com>
2025-10-29 16:47:30 +0300

    include auth router in urls

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 from core.config import settings
+from .auth import router as auth_router
 
 api_v1_router = APIRouter(
     prefix=settings.api.v1.prefix,
     tags=["V1"],
 )
+
+api_v1_router.include_router(router=auth_router)
