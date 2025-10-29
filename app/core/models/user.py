86b0802f94a479035277a3e01cb6b20f7cd00661
HELAFUG <loxand42@gmail.com>
2025-10-29 13:43:42 +0300

    made a user class

---
+++ app/core/models/user.py
@@ -0,0 +1,15 @@
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from typing import TYPE_CHECKING
+from .mixins import IdIntMixin
+from .base import Base
+from core.types.user_id import UserIdType
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class User(Base, IdIntMixin, SQLAlchemyBaseUserTable[UserIdType]):
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, cls)
