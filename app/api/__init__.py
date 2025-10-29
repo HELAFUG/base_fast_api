226c326e65c941efa67d377092b8835a322a8b70
HELAFUG <loxand42@gmail.com>
2025-10-29 16:17:45 +0300

    add first version of router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 from core.config import settings
+from .api_v1 import api_v1_router
 
 router = APIRouter(
     prefix=settings.api.pretix,
     tags=["API"],
 )
+
+router.include_router(router=api_v1_router)
---
+++ app/api/api_v1/__init__.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from core.config import settings
+
+api_v1_router = APIRouter(
+    prefix=settings.api.v1.prefix,
+    tags=["V1"],
+)
