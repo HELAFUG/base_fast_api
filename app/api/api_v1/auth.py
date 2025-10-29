e35a79fc4153e9ba237ac9c736428d5d62c79193
HELAFUG <loxand42@gmail.com>
2025-10-29 16:46:44 +0300

    made auth router

---
+++ app/api/api_v1/auth.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from core.config import settings
+from dependencies.authentication import authentication_backend
+from .fastapi_users import fastapi_users
+
+
+router = APIRouter(
+    prefix=settings.api.v1.auth,
+    tags=["Auth"],
+)
+
+router.include_router(router=fastapi_users.get_auth_router(authentication_backend))
