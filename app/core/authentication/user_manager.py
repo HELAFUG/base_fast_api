3ab7ddf0be8c08a1b0f78852279737c0aa646eb8
HELAFUG <loxand42@gmail.com>
2025-10-29 17:38:14 +0300

    meanwhile my mixin....

--- app/core/authentication/user_manager.py
+++ app/core/authentication/user_manager.py
@@ -1,10 +1,11 @@
 from fastapi import Depends
-from fastapi_users import BaseUserManager, UUIDIDMixin
+from fastapi_users import BaseUserManager
 from typing import Optional
 from typing import TYPE_CHECKING
 from core.models import User
 from core.types.user_id import UserIdType
 from core.config import settings
+from core.models.mixins import IdIntMixin
 import logging
 
 
@@ -14,7 +15,7 @@ if TYPE_CHECKING:
 log = logging.getLogger(__name__)
 
 
-class UserManager(UUIDIDMixin, BaseUserManager[User, UserIdType]):
+class UserManager(IdIntMixin, BaseUserManager[User, UserIdType]):
     reset_password_token_secret = settings.access_token.reset_password_token_secret
     verification_token_secret = settings.access_token.verification_token_secret
 
