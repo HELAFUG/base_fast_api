82b4fecf21129da45e696a95386267b6833475c7
HELAFUG <loxand42@gmail.com>
2025-10-30 06:21:32 +0300

    add message router(example)

--- app/api/api_v1/messages.py
+++ app/api/api_v1/messages.py
@@ -1,8 +1,25 @@
 from fastapi import APIRouter, Depends
-from core.config import settings
+from typing import Annotated
+from core.models import User
 
+from core.config import settings
+from core.schemas.user import UserRead
+from .fastapi_users import (
+    current_user,
+    current_superuser,
+)
 
 router = APIRouter(
     prefix=settings.api.v1.messages,
     tags=["Messages"],
 )
+
+
+@router.get("")
+def get_user_messages(user: Annotated[User, Depends(current_user)]):
+    return {"messages": UserRead.model_validate(user)}
+
+
+@router.get("/secrets")
+def get_superuser_messages(user: Annotated[User, Depends(current_superuser)]):
+    return {"user": UserRead.model_validate(user)}
