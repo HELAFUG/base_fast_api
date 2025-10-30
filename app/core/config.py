47060ae75aa81c117042de432c5f90ed7837a80b
HELAFUG <loxand42@gmail.com>
2025-10-30 05:32:34 +0300

    add base dir path in config

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,12 @@
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 
 load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 class DBSettings(BaseModel):
