76f257b8375c5f6907e9e54d343b58c72c6b9675
HELAFUG <loxand42@gmail.com>
2025-11-06 20:51:10 +0300

    add body data

--- app/api/utils/mail/send_email.py
+++ app/api/utils/mail/send_email.py
@@ -6,13 +6,13 @@ from core.config import settings
 ADMIN_EMAIL = getenv("ADMIN_EMAIL", "admin@example.com")
 
 
-async def send_email(recepient: str, receier: str, sub: str):
+async def send_email(recepient: str, body: str, sub: str):
 
     message = EmailMessage()
     message["Subject"] = sub
     message["From"] = ADMIN_EMAIL
     message["To"] = recepient
-    message.set_content()
+    message.set_content(body)
 
     await aiosmtplib.send(
         message,
