d533272092bf64cdcc5d2a6913d2f6d7f5a0d731
HELAFUG <loxand42@gmail.com>
2025-11-07 12:40:25 +0300

    add forgot password func and after

--- app/api/utils/password/forgot.py
+++ app/api/utils/password/forgot.py
@@ -8,3 +8,11 @@ async def forgot_password_email(user: User, token: str):
         sub="Reset Password Token Inside, Please Be Cautious",
         body=token,
     )
+
+
+async def after_reset_password(user: User):
+    await send_email(
+        recepient=user.email,
+        sub="Reset Password Process",
+        body="Your password has been reset",
+    )
--- app/core/authentication/user_manager.py
+++ app/core/authentication/user_manager.py
@@ -10,7 +10,7 @@ from core.types.user_id import UserIdType
 from core.config import settings
 from api.utils.mail.welcome import welcome_email
 from api.utils.user.verify import verify_email, after_verify
-
+from api.utils.password.forgot import forgot_password_email, after_reset_password
 
 if TYPE_CHECKING:
     from fastapi import Request
@@ -54,5 +54,12 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
         request: Optional["Request"] = None,
     ):
         log.warning("Forgot password requested for user %r, token %r", user.id, token)
+        await forgot_password_email(user, token)
 
-    # TODO: make this work
+    async def on_after_reset_password(
+        self,
+        user: User,
+        request: Optional["Request"] = None,
+    ):
+        log.warning("Password has been reset for user %r", user.id)
+        await after_reset_password(user)
