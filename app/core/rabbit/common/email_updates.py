a8e7c14aeadb743131320eaa3e76e6779617ad36
HELAFUG <loxand42@gmail.com>
2025-11-10 21:19:18 +0300

    add new method

--- app/core/rabbit/common/email_updates.py
+++ app/core/rabbit/common/email_updates.py
@@ -15,3 +15,23 @@ class EmailUpdatesRabbitMixin:
             exchange=config.MQ_EMAIL_UPDATES_EXCHANGE_NAME,
             exchange_type=ExchangeType.fanout,
         )
+
+    def declare_queue_for_email_request(
+        self,
+        queue_name: str = "",
+        exclusive: bool = True,
+    ) -> str:
+        self.declare_email_updates_exchange()
+        queue = self.channel.queue_declare(
+            queue=queue_name,
+            exclusive=exclusive,
+        )
+
+        q_name = queue_name.method.queue
+
+        self.channel.queue_bind(
+            exchange=config.MQ_EMAIL_UPDATES_EXCHANGE_NAME,
+            queue=q_name,
+        )
+
+        return q_name
