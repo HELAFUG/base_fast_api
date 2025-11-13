fadbc55a988972fa07632734c5d7b7ba9bc3f8ae
HELAFUG <loxand42@gmail.com>
2025-11-13 13:24:23 +0300

    some minor changes

--- app/mailing/mail/send_welcome_email.py
+++ app/mailing/mail/send_welcome_email.py
@@ -1,8 +1,12 @@
+from core.models import db_helper
 from core.models import User
+from crud.user import get_user_by_id
 from .send_email import send_email
 
 
-async def send_welcome_email(user: User) -> None:
+async def send_welcome_email(user_id: int) -> None:
+    async with db_helper.session_factory() as session:
+        user = await get_user_by_id(session=session, user_id=user_id)
     await send_email(
         recipient=user.email,
         sub="Welcome to FastAPI",
