2cc87949a7d9f3873f1e6b7056715738a075dd27
HELAFUG <loxand42@gmail.com>
2025-11-14 12:18:47 +0300

    add events in broker

--- app/core/taskiq_broker.py
+++ app/core/taskiq_broker.py
@@ -1,7 +1,24 @@
 __all__ = ("broker",)
 
-
+import logging
+from taskiq import TaskiqEvents, TaskiqState
 from taskiq_aio_pika import AioPikaBroker
 from core.config import settings
 
+log = logging.getLogger(__name__)
+
 broker = AioPikaBroker(url=settings.taskiq.url)
+
+
+@broker.on_event(TaskiqEvents.WORKER_STARTUP)
+async def on_worker_start_up(state: TaskiqState) -> None:
+    logging.basicConfig(
+        level=settings.logging.log_level,
+        datefmt=settings.logging.datefmt,
+    )
+    log.info("Worker started up: %r", state)
+
+
+@broker.on_event(TaskiqEvents.WORKER_SHUTDOWN)
+async def on_worker_shut_down(state: TaskiqState) -> None:
+    log.info("Worker shut down: %r", state)
