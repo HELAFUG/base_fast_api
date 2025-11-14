d36c4c6f0a3324ecbfae4057e452013f369034cc
HELAFUG <loxand42@gmail.com>
2025-11-14 12:43:21 +0300

    adding dependencies in broker

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -6,8 +6,8 @@ from core.config import settings
 from .welcome_email_notification import welcome_email_notification
 
 
-if sys.argv[0] == "worker":
-    logging.basicConfig(
-        level=settings.logging.log_level,
-        datefmt=settings.logging.datefmt,
-    )
+# if sys.argv[0] == "worker":
+#     logging.basicConfig(
+#         level=settings.logging.log_level,
+#         datefmt=settings.logging.datefmt,
+#     )
