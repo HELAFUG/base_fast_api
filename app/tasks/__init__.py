de5ae7266057e8e620dd7dc27faab63e61f620bd
HELAFUG <loxand42@gmail.com>
2025-11-14 12:13:52 +0300

    add logging in workers system

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,3 +1,13 @@
 __all__ = ("welcome_email_notification",)
 
+import sys
+import logging
+from core.config import settings
 from .welcome_email_notification import welcome_email_notification
+
+
+if sys.argv[0] == "worker":
+    logging.basicConfig(
+        level=settings.logging.log_level,
+        datefmt=settings.logging.datefmt,
+    )
