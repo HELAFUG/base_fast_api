3c07adcb435e55f7da6a03f9732c1d7026cb9268
HELAFUG <loxand42@gmail.com>
2025-11-17 17:18:11 +0300

    oh i forgot token

--- app/tasks/passwords.py
+++ app/tasks/passwords.py
@@ -5,7 +5,8 @@ from taskiq import TaskiqDepends
 from core import broker
 from core.models import User, db_helper
 from crud.user import get_user_by_id
-from mailing.passwords.forgot import send_password_forgot_email as send
+from mailing.passwords.forgot import send_password_forgot_email
+from mailing.passwords.reset import send_reset_password_email
 
 log = logging.getLogger(__name__)
 
@@ -13,11 +14,12 @@ log = logging.getLogger(__name__)
 @broker.task
 async def send_after_forgot(
     user_id: int,
+    token: str,
     session: Annotated[AsyncSession, TaskiqDepends(db_helper.session_getter)],
 ):
     user: User = await get_user_by_id(session=session, user_id=user_id)
     log.info("Sending password forgot email to user %r", user_id)
-    await send(user=user)
+    await send_password_forgot_email(user=user, token=token)
 
 
 @broker.task
@@ -27,4 +29,4 @@ async def send_after_reset(
 ):
     user: User = await get_user_by_id(session=session, user_id=user_id)
     log.info("Sending password reset email to user %r", user_id)
-    await send(user=user)
+    await send_reset_password_email(user=user)
