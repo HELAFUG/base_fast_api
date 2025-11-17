a01d90356621b0e2db77d83e28dffa534cb100f2
HELAFUG <loxand42@gmail.com>
2025-11-17 17:51:28 +0300

    add to init

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -2,6 +2,9 @@ __all__ = (
     "welcome_email_notification",
     "send_after_forgot",
     "send_after_reset",
+    "send_after_login",
+    "send_after_verify",
+    "on_after_success",
 )
 
 import sys
@@ -9,7 +12,7 @@ import logging
 from core.config import settings
 from .welcome_email_notification import welcome_email_notification
 from .passwords import send_after_forgot, send_after_reset
-
+from .user import send_after_login, send_after_verify, on_after_success
 
 # if sys.argv[0] == "worker":
 #     logging.basicConfig(
