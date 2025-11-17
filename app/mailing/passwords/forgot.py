ff51a268b3a43ec9a5561655625983533ab95412
HELAFUG <loxand42@gmail.com>
2025-11-17 17:20:46 +0300

    soem changes in message

--- app/mailing/passwords/forgot.py
+++ app/mailing/passwords/forgot.py
@@ -5,6 +5,6 @@ from core.models import User
 async def send_password_forgot_email(user: User, token: str):
     await send_email(
         recipient=user.email,
-        sub="Verification Token,Warning Dont Share",
-        body=token,
+        sub="Verification Token Inside,Warning Dont Share",
+        body=f"your personal reset token is {token}",
     )
