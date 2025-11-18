96fea76cd20610fb25e2f22b8eb23c368f7e2bc5
HELAFUG <loxand42@gmail.com>
2025-11-18 14:56:13 +0300

    some reload in redis init

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis.asyncio import Redis
 from contextlib import asynccontextmanager
 from core.config import settings
 from core.models import db_helper
@@ -11,6 +14,15 @@ logging.basicConfig(format=settings.logging.log_format)
 
 @asynccontextmanager
 async def lifespan(app):
+    redis = Redis(
+        host=settings.redis.host,
+        port=settings.redis.port,
+        db=settings.redis.db.cache,
+    )
+    FastAPICache.init(
+        RedisBackend(redis),
+        prefix=settings.cache.prefix,
+    )
     if not broker.is_worker_process:
         await broker.startup()
     yield
