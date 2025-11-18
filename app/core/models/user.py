58c5167bf0f57d94000b833f5b57fde933fadaff
HELAFUG <loxand42@gmail.com>
2025-11-18 15:50:35 +0300

    add custom method to user table

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,6 +1,9 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase as SQLAlchemyUserDatabaseGeneric,
+)
 from sqlalchemy.orm import Mapped, relationship
-
+from sqlalchemy import select
 from typing import TYPE_CHECKING
 from .mixins import IdIntMixin
 from .base import Base
@@ -9,6 +12,14 @@ from core.types.user_id import UserIdType
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
     from .access_token import AccessToken
+    from .user import User
+
+
+class SQLAlchemyUserDatabase(SQLAlchemyUserDatabaseGeneric):
+    async def get_users(self) -> list["User"]:
+        stmt = select(User).order_by(User.id)
+        res = await self.session.scalars(stmt)
+        return list(res.all())
 
 
 class User(Base, IdIntMixin, SQLAlchemyBaseUserTable[UserIdType]):
