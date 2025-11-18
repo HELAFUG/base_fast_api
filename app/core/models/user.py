889b8cd7c88bc71af200520798001502e0e02bf0
HELAFUG <loxand42@gmail.com>
2025-11-18 15:43:51 +0300

    some reload in db schema and migrations

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,4 +1,6 @@
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.orm import Mapped, relationship
+
 from typing import TYPE_CHECKING
 from .mixins import IdIntMixin
 from .base import Base
@@ -6,13 +8,17 @@ from core.types.user_id import UserIdType
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+    from .access_token import AccessToken
 
 
 class User(Base, IdIntMixin, SQLAlchemyBaseUserTable[UserIdType]):
+    access_tokens: Mapped[list["AccessToken"]] = relationship(
+        back_populates="user",
+    )
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, cls)
 
     def __str__(self):
-        return self.id
+        return self.email
