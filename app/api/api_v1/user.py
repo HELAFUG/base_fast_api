da76259f8071b35f491a7bfcfad7f9da3fe0c378
HELAFUG <loxand42@gmail.com>
2025-11-18 15:56:07 +0300

    add new handler in user router

--- app/api/api_v1/user.py
+++ app/api/api_v1/user.py
@@ -3,9 +3,14 @@ from fastapi import (
     Depends,
 )
 from fastapi.security import HTTPBearer
+from typing import Annotated, TYPE_CHECKING
 from core.config import settings
 from core.schemas.user import UserRead, UserUpdate
+from api.dependencies.authentication.users import get_users_db
 from .fastapi_users import fastapi_users
+
+if TYPE_CHECKING:
+    from core.authentication.user_manager import SQLAlchemyUserDatabase
 
 http_bearer = HTTPBearer(auto_error=False)
 
@@ -14,6 +19,12 @@ router = APIRouter(
     tags=["Users"],
     dependencies=[Depends(http_bearer)],
 )
+
+
+@router.get("sex", response_model=list[UserRead])
+async def get_all(users_db: Annotated["SQLAlchemyUserDatabase", Depends(get_users_db)]):
+    return await users_db.get_users()
+
 
 router.include_router(
     fastapi_users.get_users_router(
